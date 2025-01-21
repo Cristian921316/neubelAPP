b4237660ecd093ed2ed6404933745e9870e76d8f
Cristian Calle <alex2020e_@hotmail.com>
2025-01-20 23:45:15 -0500

    Grafico pastel pantalla inicio

---
+++ BlazorApp2/wwwroot/chart-config.js
@@ -0,0 +1,27 @@
+﻿// Método para crear el gráfico de pastel
+function crearGraficoPastel(labels, data) {
+    var ctx = document.getElementById('graficoPastel').getContext('2d');
+    new Chart(ctx, {
+        type: 'pie', // Tipo de gráfico: pastel
+        data: {
+            labels: labels, // Etiquetas dinámicas
+            datasets: [{
+                label: 'Distribución',
+                data: data, // Datos dinámicos
+                backgroundColor: ['#36A2EB', '#ff5c00'], // Colores
+                hoverOffset: 4
+            }]
+        },
+        options: {
+            responsive: true,
+            plugins: {
+                legend: {
+                    position: 'top'
+                },
+                tooltip: {
+                    enabled: true
+                }
+            }
+        }
+    });
+}
